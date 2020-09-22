1ec3b9905d31109141279265d1f1a5c93723e99e
Matthew Inamdar <matt@goldspecdigital.com>
2020-09-22 12:58:38 +0100

    Updated deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
 # Requires the following environment variables:
-# $1 = The environment (production/release/staging).
-# $2 = The URI of the ECR repo to push to.
-# $3 = The name of the ECS cluster to deploy to.
-# $4 = The AWS access key.
-# $5 = The AWS secret access key.
-# $6 = The AWS region.
+# $ENVIRONMENT = The environment (production/release/staging).
+# $REPO_URI = The URI of the ECR repo to push to.
+# $CLUSTER = The name of the ECS cluster to deploy to.
+# $AWS_ACCESS_KEY_ID = The AWS access key.
+# $AWS_SECRET_ACCESS_KEY = The AWS secret access key.
+# $AWS_DEFAULT_REGION = The AWS region.
+# $TRAVIS_BUILD_DIR = The directory of the project.
+# $TRAVIS_COMMIT = The commit hash of the build.
 
 # Bail out on first error.
 set -e
 
 # Set environment variables.
-export ENV_SECRET_ID=".env.api.${1}"
-export REPO_URI=${2}
-export CLUSTER=${3}
-export AWS_ACCESS_KEY_ID=${4}
-export AWS_SECRET_ACCESS_KEY=${5}
-export AWS_DEFAULT_REGION=${6}
+echo "Setting deployment configuration for ${ENVIRONMENT}..."
+export ENV_SECRET_ID=".env.api.${ENVIRONMENT}"
 
 # Build the image.
 ./docker/build.sh
