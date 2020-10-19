b09b4e44a03e56ea91cef9855a73664196e59311
Stuart Laverick <appsol@users.noreply.github.com>
2020-10-19 07:41:18 +0100

    feature/add-link-to-organisation-welcome-email (#38)

--- app/Emails/OrganisationAdminInviteFirstFollowUps/NotifyInviteeEmail.php
+++ app/Emails/OrganisationAdminInviteFirstFollowUps/NotifyInviteeEmail.php
@@ -21,8 +21,8 @@ class NotifyInviteeEmail extends Email
     {
         return <<<'EOT'
 Dear ((ORGANISATION_NAME)),
-I’m writing to invite you to claim a listing for ((ORGANISATION_NAME)) on NHS Connect. 
-Your organisation has been recommended to us as an important source of support and we would like to add you to our new online directory as an early priority.  Your listing will then be accessible to citizens, to professionals supporting citizens (such as GPs or Link Workers), as well as to funders and commissioners. 
+I’m writing to invite you to claim a listing for ((ORGANISATION_NAME)) on NHS Connect.
+Your organisation has been recommended to us as an important source of support and we would like to add you to our new online directory as an early priority.  Your listing will then be accessible to citizens, to professionals supporting citizens (such as GPs or Link Workers), as well as to funders and commissioners.
 
 We currently have your organisation listed as:
 Address: ((ORGANISATION_ADDRESS))
@@ -32,17 +32,15 @@ Phone: ((ORGANISATION_PHONE))
 Social Media: ((ORGANISATION_SOCIAL_MEDIA))
 Description: ((ORGANISATION_DESCRIPTION))
 
-Click here to claim your listing:
-((INVITE_URL))
+<a href="((INVITE_URL))">Click here</a> to claim your listing.
 
 The Connect directory has been developed by the NHS to maximise everyone’s access to support for health and wellbeing. Adding a listing is completely free, improves your organisation’s profile, gives you access to new funding opportunities and above all ensures that the people who need your support most are able to access it.
 
-Not your organisation?  
-Our initial information is not always accurate and nothing you see here is made public until it is confirmed by you. If you want to add details for another organisation click here. 
-https://admin.connect.nhs.uk/register/
+Not your organisation?
+Our initial information is not always accurate and nothing you see here is made public until it is confirmed by you. If you want to add details for another organisation <a href="https://admin.connect.nhs.uk/register/">click here</a>.
 
-Having problems? 
-You can view some Frequently Asked Questions here
+Having problems?
+You can view some <a href="https://connect.nhs.uk/providers">Frequently Asked Questions here</a>.
 
 Many thanks for your help
 
