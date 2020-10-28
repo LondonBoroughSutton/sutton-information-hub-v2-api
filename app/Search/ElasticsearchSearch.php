9cdf65ea425aa06e2c808ed9da8e78284e0bcc01
Stuart Laverick <appsol@users.noreply.github.com>
2020-10-28 21:36:44 +0000

    feature/search-within-collection-set (#37)
    * Set up tests
    
    * Changed category and persona search to accept a comma seperated list of strings
    
    * Fixed unit test. Collections filtered on slug not name now
    
    * Combined ElasticsearchSearch::applyCategory and applyPersona into one method apply Collection
    
    * Refactored ElasticsearchSearch to previous api and delegate to protected method
    
    Co-authored-by: Matt Inamdar <matt@goldspecdigital.com>

--- app/Search/ElasticsearchSearch.php
+++ app/Search/ElasticsearchSearch.php
@@ -139,25 +139,7 @@ class ElasticsearchSearch implements Search
      */
     public function applyCategory(string $category): Search
     {
-        $categoryModel = CollectionModel::query()
-            ->with('taxonomies')
-            ->categories()
-            ->where('name', $category)
-            ->firstOrFail();
-
-        $should = &$this->query['query']['bool']['must']['bool']['should'];
-
-        foreach ($categoryModel->taxonomies as $taxonomy) {
-            $should[] = $this->match('taxonomy_categories', $taxonomy->name);
-        }
-
-        $this->query['query']['bool']['filter']['bool']['must'][] = [
-            'term' => [
-                'collection_categories' => $category,
-            ],
-        ];
-
-        return $this;
+        return $this->applyCollection($category, 'category');
     }
 
     /**
@@ -165,25 +147,7 @@ class ElasticsearchSearch implements Search
      */
     public function applyPersona(string $persona): Search
     {
-        $categoryModel = CollectionModel::query()
-            ->with('taxonomies')
-            ->personas()
-            ->where('name', $persona)
-            ->firstOrFail();
-
-        $should = &$this->query['query']['bool']['must']['bool']['should'];
-
-        foreach ($categoryModel->taxonomies as $taxonomy) {
-            $should[] = $this->match('taxonomy_categories', $taxonomy->name);
-        }
-
-        $this->query['query']['bool']['filter']['bool']['must'][] = [
-            'term' => [
-                'collection_personas' => $persona,
-            ],
-        ];
-
-        return $this;
+        return $this->applyCollection($persona, 'persona');
     }
 
     /**
@@ -422,5 +386,51 @@ class ElasticsearchSearch implements Search
                 return $location->distanceFrom($serviceLocation->location->toCoordinate());
             });
         });
+    }
+
+    /**
+     * @param string $slug
+     * @param string $type
+     * @return \App\Search\ElasticsearchSearch
+     */
+    protected function applyCollection(string $slug, string $type): Search
+    {
+        $query = CollectionModel::query()
+            ->with('taxonomies')
+            ->where('slug', $slug);
+
+        if ($type === 'category') {
+            $query->categories();
+        } elseif ($type === 'persona') {
+            $query->personas();
+        } else {
+            throw new \Exception('Invalid Collection Type');
+        }
+
+        $collectionModel = $query->firstOrFail();
+
+        $term = $type === 'category' ? 'collection_categories' : 'collection_personas';
+
+        $should = &$this->query['query']['bool']['must']['bool']['should'];
+
+        foreach ($collectionModel->taxonomies as $taxonomy) {
+            $should[] = $this->match('taxonomy_categories', $taxonomy->name);
+        }
+
+        foreach ($this->query['query']['bool']['filter']['bool']['must'] as &$filter) {
+            if (is_array($filter) && array_key_exists('terms', $filter) && array_key_exists($term, $filter['terms'])) {
+                $filter['terms'][$term][] = $collectionModel->name;
+
+                return $this;
+            }
+        }
+
+        $this->query['query']['bool']['filter']['bool']['must'][] = [
+            'terms' => [
+                $term => [$collectionModel->name],
+            ],
+        ];
+
+        return $this;
     }
 }
