5526c1635ab7706413b2637ff85cc598197b778f
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2020-11-06 13:00:10 +0000

    Allow jpg/jpeg logos on Services

--- app/Http/Requests/Service/StoreRequest.php
+++ app/Http/Requests/Service/StoreRequest.php
@@ -211,7 +211,7 @@ class StoreRequest extends FormRequest
             'gallery_items.*.file_id' => [
                 'required_with:gallery_items.*',
                 'exists:files,id',
-                new FileIsMimeType(File::MIME_TYPE_PNG, File::MIME_TYPE_JPG),
+                new FileIsMimeType(File::MIME_TYPE_PNG, File::MIME_TYPE_JPG, File::MIME_TYPE_JPEG),
                 new FileIsPendingAssignment(),
             ],
 
@@ -220,7 +220,7 @@ class StoreRequest extends FormRequest
             'logo_file_id' => [
                 'nullable',
                 'exists:files,id',
-                new FileIsMimeType(File::MIME_TYPE_PNG),
+                new FileIsMimeType(File::MIME_TYPE_PNG, File::MIME_TYPE_JPG, File::MIME_TYPE_JPEG),
                 new FileIsPendingAssignment(),
             ],
         ];
