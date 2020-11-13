62141aa1ce4fd00c72611c0376284294d3cfecfb
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2020-11-13 17:19:29 +0000

    Moved category and Persona specific files to shared locations

---
+++ app/Http/Requests/Collection/Category/StoreRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\Collection\Category;
+
+use App\Models\Collection;
+use App\Models\File;
+use App\Models\Taxonomy;
+use App\Rules\FileIsMimeType;
+use App\Rules\FileIsPendingAssignment;
+use App\Rules\RootTaxonomyIs;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($this->user()->isSuperAdmin()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'min:1', 'max:255'],
+            'intro' => ['required', 'string', 'min:1', 'max:255'],
+            'order' => ['required', 'integer', 'min:1', 'max:' . (Collection::categories()->count() + 1)],
+            'sideboxes' => ['present', 'array', 'max:3'],
+            'sideboxes.*' => ['array'],
+            'sideboxes.*.title' => ['required_with:sideboxes.*', 'string'],
+            'sideboxes.*.content' => ['required_with:sideboxes.*', 'string'],
+            'category_taxonomies' => ['present', 'array'],
+            'category_taxonomies.*' => ['string', 'exists:taxonomies,id', new RootTaxonomyIs(Taxonomy::NAME_CATEGORY)],
+            'image_file_id' => [
+                'exists:files,id',
+                new FileIsMimeType(File::MIME_TYPE_PNG),
+                new FileIsPendingAssignment(),
+            ],
+        ];
+    }
+}
