9e18f43fea06d81781a3d29abcd3e2eb9295a547
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-01-18 19:03:36 +0000

    Migration for collection_page pivot table

---
+++ database/factories/CollectionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Collection;
+use Faker\Generator as Faker;
+
+$factory->define(Collection::class, function (Faker $faker) {
+    return [
+        'type' => Collection::TYPE_CATEGORY,
+        'name' => $faker->sentence(2),
+        'meta' => [
+            "intro" => $faker->sentence,
+            "subtitle" => $faker->sentence,
+            "sideboxes" => [],
+            "image_file_id" => null,
+        ],
+        'order' => $faker->numberBetween(1, 5),
+        'enabled' => true,
+    ];
+});
+
+$factory->state(Collection::class, 'typePersona', [
+    'type' => Collection::TYPE_PERSONA,
+]);
